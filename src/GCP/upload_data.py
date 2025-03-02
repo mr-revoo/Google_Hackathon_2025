39c3620f3c2b856bbad9b542762fd722c75aced7
refaat <refaatmohamed61@gmail.com>
2025-03-02 06:06:38 +0200

    Finalizma :)

--- src/GCP/upload_data.py
+++ src/GCP/upload_data.py
@@ -1,13 +1,12 @@
 import os
 from google.cloud import bigquery
-
 # Set the environment variable
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/youssef/Desktop/GDG/startups-452221-a288712043d0.json"
 
 client = bigquery.Client()
 
-dataset_id = "startups-452221.america_dataset"
-table_id = f"{dataset_id}.investment"
+DATASET_ID = "startups-452221.america_dataset"
+table_id = f"{DATASET_ID}.investment"
 
 job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
@@ -19,4 +18,5 @@ with open("cleaned_investments_VC.csv", "rb") as source_file:
     job = client.load_table_from_file(source_file, table_id, job_config=job_config)
 
 job.result()  # Waits for the job to complete
+
 print(f"Loaded {job.output_rows} rows into {table_id}.")
